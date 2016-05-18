f415e959e81fa44985cc143e12b109677b8acbe8
tiandi0228 <tiandi0228@live.com>
2016-05-18 22:26:46 +0800

    init

--- src/views/Create/Create.jsx
+++ src/views/Create/Create.jsx
@@ -5,78 +5,102 @@ import request from 'superagent';
 
 // 创建任务
 const Create = React.createClass({
-    
-    getInitialState () {
-      return {
-          prices: '1'
+
+    getInitialState() {
+        return {
+            prices: '1'
         };
     },
-     
-    handleChange () {
-        const prices = this.refs.num.value;
-        const n = prices * 0.0001;
-        this.setState({prices: n.toFixed(2)});
+
+    handleChange() {
+        const num = this.refs.num.value;
+        const price = this.refs.price.value;
+        if (price != '0') {
+            const n = num * price;
+            this.setState({ prices: n.toFixed(2) });
+        }
     },
-    
+
     // 关闭窗口
     close() {
         const oCreate = document.getElementById('create');
         const oMake = document.getElementById('make');
-        oCreate.className = 'create';
-        oMake.style.display = 'none';
+        clearInterval(oCreate.timer);
+        oCreate.timer = setInterval(function () {
+            if (oCreate.offsetTop == -300) {
+                clearInterval(oCreate.timer);
+            } else {
+                var iSpeed = (-300 - oCreate.offsetTop) / 6;
+                iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+                oCreate.style.top = oCreate.offsetTop + iSpeed + 'px';
+                oMake.style.display = 'none';
+            }
+        }, 100)
     },
-    
+
     handleSubmit(e) {
         e.preventDefault();
-        const oPrice = document.getElementById('price');
-        const oUrl = document.getElementById('url');
-        const oNumber = document.getElementById('number');
-        const isUrl =  /^((https|http|ftp|rtsp|mms)?:\/\/)[^\s]+/g;
+        const price = this.refs.price;
+        const url = this.refs.url;
+        const num = this.refs.num;
+        const isUrl = /^((https|http|ftp|rtsp|mms)?:\/\/)[^\s]+/g;
         const isNumber = /^\d+$/;
         const post = {
-            price: this.refs.price.value,
-            url: this.refs.url.value,
-            num: this.refs.num.value
+            price: price.value,
+            url: url.value,
+            num: num.value
         }
-        
+
         // 判断选择项目
-        if(post['price'] === "0"){
-            oPrice.nextSibling.innerHTML = '请选择项目';
-            oPrice.nextSibling.style.color = 'red';
-        }else{
-            oPrice.nextSibling.innerHTML = '';
-            oPrice.nextSibling.style.color = '';
+        if (post['price'] === "0") {
+            price.nextSibling.innerHTML = '请选择项目';
+            price.nextSibling.style.color = 'red';
+        } else if (post['price'] != "0") {
+            const n = post['num'] * post['price'];
+            this.setState({ prices: n.toFixed(2) });
+        } else {
+            price.nextSibling.innerHTML = '';
+            price.nextSibling.style.color = '';
         }
-        
+
         // 判断视频地址
-        if(!isUrl.test(post['url'])){
-            oUrl.nextSibling.innerHTML = '请输入视频地址';
-            oUrl.nextSibling.style.color = 'red';
-        }else{
-            oUrl.nextSibling.innerHTML = '';
-            oUrl.nextSibling.style.color = '';
+        if (!isUrl.test(post['url'])) {
+            url.nextSibling.innerHTML = '请输入视频地址';
+            url.nextSibling.style.color = 'red';
+        } else {
+            url.nextSibling.innerHTML = '';
+            url.nextSibling.style.color = '';
         }
-        
+
         // 判断数量
-        if(!isNumber.test(post['num'])){
-            oNumber.nextSibling.innerHTML = '请输入整数';
-            oNumber.nextSibling.style.color = 'red';
-        }else if(post['num']<10000){
-            oNumber.nextSibling.innerHTML = '最小数量是10000';
-            oNumber.nextSibling.style.color = 'red';
-        } else if(post['num']>10000000){
-            oNumber.nextSibling.innerHTML = '最大数量是10000000';
-            oNumber.nextSibling.style.color = 'red';
-        }else{
-            oNumber.nextSibling.innerHTML = '';
-            oNumber.nextSibling.style.color = '';
+        if (!isNumber.test(post['num'])) {
+            num.nextSibling.innerHTML = '请输入整数';
+            num.nextSibling.style.color = 'red';
+        } else if (post['num'] < 10000) {
+            num.nextSibling.innerHTML = '最小数量是10000';
+            num.nextSibling.style.color = 'red';
+        } else if (post['num'] > 10000000) {
+            num.nextSibling.innerHTML = '最大数量是10000000';
+            num.nextSibling.style.color = 'red';
+        } else {
+            num.nextSibling.innerHTML = '';
+            num.nextSibling.style.color = '';
         }
+
+        // AJAX
+        request
+            .get('video.json')
+            .end(function (err, res) {
+                if (err) throw err;
+                var price = JSON.parse(res.text).price;
+                this.setState({ data: price });
+            }.bind(this));
     },
-    
+
     handleReset() {
         document.form.reset();
     },
-    
+
     render() {
         const styles = require('./Create.css');
         return (
@@ -87,25 +111,25 @@ const Create = React.createClass({
                         <form name="form" id="form" onSubmit={this.handleSubmit}>
                             <div className="ipt">
                                 <label className="label-name">选择项目：</label>
-                                <select defaultValue="0" ref="price" id="price">
+                                <select defaultValue="0" ref="price">
                                     <option value="0">请选择项目</option>
-                                    <option value="0.0001" price="0.0001">优酷刷访问数[带指数]1元/1万</option>
+                                    <option value="0.0001">优酷刷访问数[带指数]1元/1万</option>
                                 </select>
                                 <span></span>
                             </div>
                             <div className="ipt">
                                 <label className="label-name">视频地址：</label>
-                                <input type="text" className="url" id="url" ref="url" placeholder="请输入视频地址" />
+                                <input type="text" className="url" ref="url" placeholder="请输入视频地址" />
                                 <span></span>
                             </div>
                             <div className="ipt">
                                 <label className="label-name">项目数量：</label>
-                                <input type="number" name="number" className="number" id="number" ref="num" defaultValue={10000} onChange={this.handleChange} />
+                                <input type="number" className="number" ref="num" defaultValue={10000} onChange={this.handleChange} />
                                 <span></span>
                             </div>
                             <div className="ipt">
                                 <label className="label-name">总共价格：</label>
-                                <span style={{color:'#ff000'}}>{this.state.prices} 元</span>
+                                <span style={{ color: '#ff0000' }}>{this.state.prices} 元</span>
                             </div>
                             <div className="ipt">
                                 <button type="submit" className="btn">添 加</button>
--- src/views/Tasks/Tasks.jsx
+++ src/views/Tasks/Tasks.jsx
@@ -8,15 +8,42 @@ import Create from './../Create/Create.jsx';
 // 任务
 class Tasks extends React.Component {
     
+    constructor() {
+        super();
+        
+        // 初始
+        this.state = {
+            data: []
+        };
+        
+        // AJAX
+        request
+            .get('video.json')
+            .end(function (err, res) {
+                if (err) throw err;
+                var notice = JSON.parse(res.text).notice;
+                this.setState({data: notice});
+            }.bind(this));
+    }
+    
     //添加
     create(e) {
         e.preventDefault();
         const oCreate = document.getElementById('create');
         const oMake = document.getElementById('make');
-        oCreate.className = 'create cur';
-        oMake.style.display = 'block';
+        clearInterval(oCreate.timer);
+        oCreate.timer = setInterval(function () {
+            if (oCreate.offsetTop == 150) {
+                clearInterval(oCreate.timer);
+            } else {
+                var iSpeed = (150 - oCreate.offsetTop) / 6;
+                iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+                oCreate.style.top = oCreate.offsetTop + iSpeed + 'px';
+                oMake.style.display = 'block';
+            }
+        }, 100)
     }
-
+    
     render() {
         const styles = require('./Tasks.css');
         return (
@@ -26,6 +53,18 @@ class Tasks extends React.Component {
                     <div className="tit">
                         <h2 className="fl">任务</h2>
                         <span className="fr" onClick={this.create}>添加</span>
+                    </div>
+                    <div className="list">
+                        <ol>
+                            <li>编号</li>
+                            <li>视频地址</li>
+                            <li>初始数量</li>
+                            <li>目标数量</li>
+                            <li>完成数量</li>
+                            <li>状态</li>
+                            <li>创建于</li>
+                            <li>操作</li>
+                        </ol>
                     </div>
                 </div>
                 <Create />
